b5e463690f0a6ed28001e33204edb32579d5be95
hhh3167253066 <3167253066@qq.com>
2022-03-26 17:21:56 +0800

    vue3组合式api的一些理解

---
+++ 前端小野森森/前端基础相关等/组合式api与optionsApi/vue3-demo/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <span>{{count}}</span>
+    <p>
+      <button @click="plus">+</button>
+      <button @click="minus">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+
+// vue3中直接支持把方法给抽离出去
+const useCompute = (count) => {
+  const plus = () => {
+    count.value++
+  }
+  const minus = () => {
+    count.value--
+  }
+
+  return {
+    plus,
+    minus
+  }
+}
+
+
+export default {
+  name: 'App',
+  setup(){
+    const count = ref(0);  // ref是个函数，体现函数式编程
+
+    const {plus, minus} = useCompute(count)
+
+    // const plus =()=> {
+    //   count.value++;
+    // }
+
+    // const minus = () => {
+    //   count.value--;
+    // }
+
+    return {
+      count,
+      plus,
+      minus
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
