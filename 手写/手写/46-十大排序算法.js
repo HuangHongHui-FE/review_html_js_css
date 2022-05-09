67f4e81366a6ea3588a27653be443c5cb879996e
hhh3167253066 <3167253066@qq.com>
2022-05-09 13:40:42 +0800

    webworker与canvas实现获取图片数据

--- 手写/手写/46-十大排序算法.js
+++ 手写/手写/46-十大排序算法.js
@@ -49,26 +49,32 @@
 
 
 
-// 快排
-function quickSort (arr, s, t) {
-  let i = s, j = t
-  if (s < t) {
-    let tmp = arr[s]
-    while (i != j) {
-      while (j > i && arr[j] <= tmp)
-        j--
-      arr[i] = arr[j]
-      while (i < j && arr[i] >= tmp)
-        i++
-      arr[j] = arr[i]
+// 归并排序
+function mergeSort(arr) {
+    var len = arr.length;
+    if (len < 2) {
+        return arr;
     }
-    arr[i] = tmp
-    quickSort(arr, s, i-1)
-    quickSort(arr, i+1, t)
-  }
-  return arr
+    var middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
 }
-
-
+function merge(left, right) {
+    var result = [];
+    while (left.length>0 && right.length>0) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    while (left.length)
+        result.push(left.shift());
+    while (right.length)
+        result.push(right.shift());
+    return result;
+}
+console.log(mergeSort([2,6,9,4,2,5,6]))
 
 
